432eff411856b43a1a959ea56b34da87418ab720
Danilo Chang <ray2501@gmail.com>
2023-12-15 01:35:28 +0800

    Update tclconfig files

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,5 +1,10 @@
+# -*- tcl -*-
+# Tcl package index file, version 1.1
 #
-# Tcl package index file
-#
-package ifneeded mpg123 1.0 \
-    [list load [file join $dir libmpg1231.0.so] mpg123]
+if {[package vsatisfies [package provide Tcl] 9.0-]} {
+    package ifneeded mpg123 1.0 \
+	    [list load [file join $dir libtcl9mpg1231.0.so] [string totitle mpg123]]
+} else {
+    package ifneeded mpg123 1.0 \
+	    [list load [file join $dir libmpg1231.0.so] [string totitle mpg123]]
+}
