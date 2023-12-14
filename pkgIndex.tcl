96583a769142548e414f15ba43a6d87d12318601
Danilo Chang <ray2501@gmail.com>
2023-12-15 01:38:19 +0800

    Bump version to 1.1

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,9 @@
 # Tcl package index file, version 1.1
 #
 if {[package vsatisfies [package provide Tcl] 9.0-]} {
-    package ifneeded mpg123 1.0 \
-	    [list load [file join $dir libtcl9mpg1231.0.so] [string totitle mpg123]]
+    package ifneeded mpg123 1.1 \
+	    [list load [file join $dir libtcl9mpg1231.1.so] [string totitle mpg123]]
 } else {
-    package ifneeded mpg123 1.0 \
-	    [list load [file join $dir libmpg1231.0.so] [string totitle mpg123]]
+    package ifneeded mpg123 1.1 \
+	    [list load [file join $dir libmpg1231.1.so] [string totitle mpg123]]
 }
